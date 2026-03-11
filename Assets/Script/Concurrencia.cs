210b840fb673da7cdfd7a71971b126e60509a325
YahirToledo <yahir.toledo.r@gmail.com>
2026-03-11 12:48:28 -0600

    Preparativos para P1

--- Assets/Script/Concurrencia.cs
+++ Assets/Script/Concurrencia.cs
@@ -28,7 +28,10 @@ public class Concurrencia : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (useSincrono) MoveSincrono();
+        if (useThread) MoveWithThread();
+        if (useTask) MoveWithTask();
+        if (useCoroutine) StartCoroutine(MoveWithCoroutine());
     }
 
     // Update is called once per frame
@@ -55,6 +58,41 @@ public class Concurrencia : MonoBehaviour
             sincronorSphere.position += Vector3.right * 0.05f;
         }
         Thread.Sleep(50);
+    }
+    //Movimiento con hilo secundario
+    void MoveWithThread() 
+    {
+        new Thread(() => 
+        {
+            for (int i = 0; i <= 100; i++) 
+            {
+                Thread.Sleep(50);
+                lock (mainThreadActions) 
+                {
+                    mainThreadActions.Enqueue(() => 
+                    {
+                        threadSphere.position += Vector3.right * 0.05f;
+                    });
+                } 
+            }
+        }).Start();
+    }
+
+    //Metodos con task asicrono
+    async void MoveWithTask() 
+    {
+        await Task.Run(() => 
+        {
+            for (int i = 0; i <= 100; i++) 
+            {
+                Thread.Sleep(50);
+                lock (mainThreadActions)
+                    mainThreadActions.Enqueue(() =>
+                    {
+                        taskSphere.position += Vector3.right * 0.05f;
+                    });
+            }
+        });
     }
 
     //Corutina
